a6e7ae78a7c4a93847c64819377a8f2053ad5c54
Eirik F <eirik.falck@gmail.com>
2020-09-13 20:10:06 +0200

    continue on runekf (task 5). somewhat stuck and having trouble understanding what to do at b and onwards.

--- 3-assignment/ExEKF/runekf.py
+++ 3-assignment/ExEKF/runekf.py
@@ -20,7 +20,7 @@ plt.close('all')
 try:
     # installed with "pip install SciencePLots" (https://github.com/garrettj403/SciencePlots.git)
     # gives quite nice plots
-    plt_styles = ['science', 'grid', 'bright']
+    plt_styles = ['science', 'ieee', 'grid', 'bright']
     plt.style.use(plt_styles)
     print(f'pyplot using style set {plt_styles}')
 except Exception as e:
@@ -106,14 +106,14 @@ print(ekf_filter)  # make use of the @dataclass automatic repr
 
 # initialize mean and covariance
 # TODO ArrayLike (list, np. array, tuple, ...) with 4 elements
-x_bar_init = np.array([0,0,0,0])
+x_bar_init = np.array([0,0,1,1])
 # TODO ArrayLike with 4 x 4 elements, hint: np.diag
 P_bar_init = np.diag([1,1,1,1])
 init_ekfstate = ekf.GaussParams(x_bar_init, P_bar_init)
 
 # estimate
-ekfpred_list, ekfupd_list = ekf_filter.estimate_sequence(Z, init_ekfstate, Ts,
-                                                         start_with_prediction=True)
+ekfpred_list, ekfupd_list = \
+    ekf_filter.estimate_sequence(Z, init_ekfstate, Ts, start_with_prediction=True)
 
 # get statistics:
 # TODO: see that you sort of understand what this does
@@ -140,16 +140,21 @@ ax3.set_title(rf'$\sigma_a = {sigma_a:.4f}$, $\sigma_z= {sigma_z:.4f}$,' + \
               f'\nRMSE(p\_p, p\_v, u\_p, u\_v) = ({RMSEs_str})')
 ax3.legend()
 
+
+import sys
+sys.exit(0)
+
 # %% Task 5 b and c
 
 # % parameters for the parameter grid
 # TODO: pick reasonable values for grid search
-# n_vals = 20  # is Ok, try lower to begin with for more speed (20*20*1000 = 400 000 KF steps)
-n_vals = 20
-sigma_a_low = np.nan
-sigma_a_high = np.nan
-sigma_z_low = np.nan
-sigma_z_high = np.nan
+# n_vals = 20  
+# is Ok, try lower to begin with for more speed (20*20*1000 = 400 000 KF steps)
+n_vals = 10
+sigma_a_low = 2
+sigma_a_high = 10
+sigma_z_low = 2
+sigma_z_high = 10
 
 # % set the grid on logscale(not mandatory)
 sigma_a_list = np.logspace(
@@ -161,73 +166,87 @@ sigma_z_list = np.logspace(
 
 dtype = stats.dtype  # assumes the last cell has been run without faults
 stats_array = np.empty((n_vals, n_vals, K), dtype=dtype)
+
 # %% run through the grid and estimate
 # ? Should be more or less a copy of the above
 for i, sigma_a in enumerate(sigma_a_list):
-    dynmod = None  # TODO
+    dynmod = dynamicmodels.WhitenoiseAccelleration(sigma_a)
     for j, sigma_z in enumerate(sigma_z_list):
-        measmod = None  # TODO
-        ekf_filter = None  # TODO
+        measmod = measurmentmodels.CartesianPosition(sigma_z)
+        ekf_filter = ekf.EKF(dynmod, measmod)
 
-        ekfpred_list, ekfupd_list = None  # TODO
-        stats_array[i, j] = None  # TODO
+        ekfpred_list, ekfupd_list = \
+            ekf_filter.estimate_sequence(Z, init_ekfstate, Ts)
+        stats_array[i, j] = ekf_filter.performance_stats_sequence(
+                K, Z=Z, ekfpred_list=ekfpred_list, ekfupd_list=ekfupd_list, 
+                X_true=Xgt[:, :4], norm_idxs=[[0, 1], [2, 3]], norms=[2, 2]
+            )
 
 # %% calculate averages
 
-# TODO, remember to use axis argument, see eg. stats_array['dists_pred'].shape
-RMSE_pred = None  # TODO
-RMSE_upd = None  # TODO
-ANEES_pred = None  # TODO mean of NEES over time
-ANEES_upd = None  # TODO
-ANIS = None  # TODO mean of NIS over time
+# remember to use axis argument, see eg. stats_array['dists_pred'].shape
+# The way done below is computationally heavy since it calculates the square
+# and then the square root of the same number. i didn't find an easier way to
+# just take the square/sqrt along a single axis
+RMSE_pred = np.sqrt(np.mean(np.square(stats_array['dists_pred']), axis=2))
+RMSE_upd = np.sqrt(np.mean(np.square(stats_array['dists_upd']), axis=2))
+ANEES_pred = np.mean(stats_array['NEESpred'], axis=2)
+ANEES_upd = np.mean(stats_array['NEESupd'], axis=2)
+ANIS = np.mean(stats_array['NIS'], axis=2)
 
-
+     
 # %% find confidence regions for NIS and plot
-confprob = np.nan  # TODO number to use for confidence interval
-CINIS = np.nan  # TODO confidence intervall for NIS, hint: scipy.stats.chi2.interval
-print(CINIS)
+confprob = 0.95  # number to use for confidence interval, 95% conf int
+# confidence intervall for NIS, hint: scipy.stats.chi2.interval
+CINIS = np.asarray(
+    scipy.stats.chi2.interval(confprob, ekf_filter.sensor_model.m*K)) / K
+print("CINIS:", CINIS)
 
 # plot
 fig4 = plt.figure(4, clear=True)
 ax4 = plt.gca(projection='3d')
 ax4.plot_surface(*np.meshgrid(sigma_a_list, sigma_z_list),
-                 ANIS, alpha=0.9)
+                 ANIS, alpha=0.8)
 ax4.contour(*np.meshgrid(sigma_a_list, sigma_z_list),
             ANIS, [1, 1.5, *CINIS, 2.5, 3], offset=0)  # , extend3d=True, colors='yellow')
 ax4.set_xlabel(r'$\sigma_a$')
 ax4.set_ylabel(r'$\sigma_z$')
 ax4.set_zlabel('ANIS')
 ax4.set_zlim(0, 10)
-ax4.view_init(30, 20)
+ax4.view_init(30, 30)
 
 # %% find confidence regions for NEES and plot
-confprob = np.nan  # TODO
-CINEES = np.nan  # TODO, not NIS now, but very similar
-print(CINEES)
+confprob = 0.95
+CINEES = np.asarray(
+    scipy.stats.chi2.interval(confprob, ekf_filter.dynamic_model.n*K)) / K
+print("CINEES:", CINEES)
 
 # plot
 fig5 = plt.figure(5, clear=True)
-ax5s = [fig5.add_subplot(1, 2, 1, projection='3d'),
-        fig5.add_subplot(1, 2, 2, projection='3d')]
+subplot_layout = (2,1)
+ax5s = [fig5.add_subplot(*subplot_layout, 1, projection='3d'),
+        fig5.add_subplot(*subplot_layout, 2, projection='3d')]
 ax5s[0].plot_surface(*np.meshgrid(sigma_a_list, sigma_z_list),
-                     ANEES_pred, alpha=0.9)
+                     ANEES_pred, alpha=0.8)
 ax5s[0].contour(*np.meshgrid(sigma_a_list, sigma_z_list),
                 ANEES_pred, [3, 3.5, *CINEES, 4.5, 5], offset=0)
 ax5s[0].set_xlabel(r'$\sigma_a$')
 ax5s[0].set_ylabel(r'$\sigma_z$')
-ax5s[0].set_zlabel('ANEES_pred')
+ax5s[0].set_zlabel('ANEES\_pred')
 ax5s[0].set_zlim(0, 50)
 ax5s[0].view_init(40, 30)
 
 ax5s[1].plot_surface(*np.meshgrid(sigma_a_list, sigma_z_list),
-                     ANEES_upd, alpha=0.9)
+                     ANEES_upd, alpha=0.8)
 ax5s[1].contour(*np.meshgrid(sigma_a_list, sigma_z_list),
                 ANEES_upd, [3, 3.5, *CINEES, 4.5, 5], offset=0)
 ax5s[1].set_xlabel(r'$\sigma_a$')
 ax5s[1].set_ylabel(r'$\sigma_z$')
-ax5s[1].set_zlabel('ANEES_upd')
+ax5s[1].set_zlabel('ANEES\_upd')
 ax5s[1].set_zlim(0, 50)
 ax5s[1].view_init(40, 30)
+
+fig5.tight_layout(h_pad=5.0)
 
 # %% see the intersection of NIS and NEESes
 fig6, ax6 = plt.subplots(num=6, clear=True)
@@ -240,7 +259,7 @@ cont_nis = ax6.contour(*np.meshgrid(sigma_a_list, sigma_z_list),
 
 for cs, l in zip([cont_upd, cont_pred, cont_nis], ['NEESupd', 'NEESpred', 'NIS']):
     for c, hl in zip(cs.collections, ['low', 'high']):
-        c.set_label(l + '_' + hl)
+        c.set_label(l + '\_' + hl)
 ax6.legend()
 ax6.set_xlabel(r'$\sigma_a$')
 ax6.set_ylabel(r'$\sigma_z$')
